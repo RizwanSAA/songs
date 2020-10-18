bbd17f9b268672fb616be95a66f81e8dce0698ef
RizwanSAA <sabaarizwan@outlook.com>
2020-10-18 22:14:45 +0530

    redux practice completed

---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+import { combineReducers } from "redux";
+
+const songsReducers = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I Want it That Way", duration: "1:45" },
+  ];
+};
+
+const favouriteTitle = () => "All Star";
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducers,
+  favouriteTitle: favouriteTitle,
+  selectedSong: selectedSongReducer,
+});
